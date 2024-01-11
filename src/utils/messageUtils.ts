11a1c0b5647961d08a600bd17cb5c371266fec4c
Haojin Yang <haojin.yang@.hpi.uni-potsdam.de>
2024-01-11 14:33:03 +0100

    fix small issue in generated content

--- src/utils/messageUtils.ts
+++ src/utils/messageUtils.ts
@@ -21,7 +21,7 @@ export const getChatGPTEncoding = (
   messages: MessageInterface[],
   model: ModelOptions
 ) => {
-  const isGpt3 = model === 'Yi-6B-Chat';
+  const isGpt3 = model === 'yi-6b-chat-w4a16g32';
 
   const msgSep = isGpt3 ? '\n' : '';
   const roleSep = isGpt3 ? '\n' : '<|im_sep|>';
